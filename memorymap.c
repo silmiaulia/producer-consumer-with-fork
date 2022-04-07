7e679486c9bb86c168f872babb6d15216fc8c55f
silmi aulia <99388988+silmiaulia@users.noreply.github.com>
2022-04-07 08:17:29 +0700

    Add files via upload

---
+++ memorymap.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>  // rand(), srand()
+#include <time.h>    // time()
+#include <unistd.h>     /* fork() and getpid() */
+#include <stdio.h>      /* printf() */
+#include <string.h>
+#include <sys/mman.h>   /* mmap()  */
+#include <unistd.h>     /* fork() and getpid() */
+#include <sys/wait.h>   /*wait*/
+#include <errno.h>	    /* errno */
+
+#define MSGSIZE n
+
+int main(int argc, char **argv) {
+    
+    	int n;
+	    scanf("%d", &n); //input banyak proses
+	    
+        int pid;
+        time_t t;
+        int sum = 0;
+
+        // /* Intializes random number generator */
+        srand((unsigned) time(&t));
+
+        printf("mulai \n");
+
+        int size = MSGSIZE * sizeof(int);
+        //buat memory map
+        void *addr = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+        printf("Mapped at %p\n", addr);
+        int *shared = addr;
+
+        switch (pid = fork()) {
+        case 0:         /*  proses anak */
+                printf("Proses anak \n");
+                //isi data di memory map
+                for(int i=0; i<n; i++){
+                    shared[i] = rand() % 20; //mengisi angka random ke array
+                }
+                break;
+        default:        /*  ortu */
+                printf("Proses ortu\n");
+                int pidWait,status;
+                /* wait sampai child selesai */
+                while (pidWait = wait(&status)) {   
+                    if (pidWait == pid)  /* child sukses selesai*/
+                        //cetak isi memory map
+                        for (int i = 0; i<MSGSIZE; i++) {
+                          printf("random number from first group process : %d \n", shared[i]);
+                          sum = sum + shared[i];
+                        }
+                        break; //keluar dari loop wait
+                    if ((pidWait == -1) && (errno != EINTR)) {
+                        /* ada error*/
+                        perror("waitpid");
+                        exit(1);
+                    }
+                }
+                printf("sum from second group process: %d\n", sum); //cetak hasil sum
+                break;
+        case -1:        /* error */
+                perror("fork");
+                exit(1);
+        }
+        exit(0);
+}
---
+++ pipe.c
@@ -0,0 +1,67 @@
+
+#include <stdlib.h>  // rand(), srand()
+#include <time.h>    // time()
+#include <unistd.h>     /* fork() and getpid() */
+#include <stdio.h>      /* printf() */
+#include <string.h>
+
+int main(){
+	
+    int n;
+    scanf("%d", &n); //input banyak proses
+
+    int fd[2]; //pipe
+
+    int pid;
+    time_t t;
+    int sum = 0;
+
+    // /* Intializes random number generator */
+    srand((unsigned) time(&t));
+
+    //create pipe
+    if (pipe(fd) < 0) {
+        exit(1); //error
+    }
+
+    //buat array
+    int writeNumber[n];
+    int readNumber[n];
+
+
+    printf("mulai \n");
+
+        switch (pid = fork()) {
+
+        case 0:         /* fork returns 0 ke proses anak */
+            printf("Proses anak \n");
+            /* tutup bagian input dari pipe */
+            close(fd[0]);
+                // tulis data ke pipe
+            for(int i=0; i<n; i++){
+                writeNumber[i] = rand() % 20; //get random number
+                write(fd[1], &writeNumber[i], sizeof(writeNumber[i])); //menulis ke pipe
+                printf("random number from first group process  : %d\n", writeNumber[i]); //print output random number
+            }
+            break;
+
+        default:        /* fork returns pid ke proses ortu */
+            printf("Proses ortu\n");
+            /* tutup bagian output dari pipe */
+            close(fd[1]);
+            // baca yang ditulis child dari pipe
+            for(int i=0; i<n; i++){
+                read(fd[0], &readNumber[i], sizeof(readNumber[i]));
+                sum = sum + readNumber[i]; //jumlahkan 
+            }
+            printf("sum from second group process: %d\n", sum);
+
+            break;
+            
+        case -1:        /* error */
+            perror("fork");
+            exit(1);
+        }
+        
+        exit(0);
+}
